76c4ea5762f8cd804e1ab9d3e7f19b628e31e7a1
Jan Huege <jan.huege@istec.de>
2022-09-14 16:06:26 +0200

    solution fizzbuzz

--- src/fizzBuzz/fizzBuzz.spec.ts
+++ src/fizzBuzz/fizzBuzz.spec.ts
@@ -1,6 +1,46 @@
+import { fizzBuzz } from "./fizzBuzz";
+
 describe("fizzbuzz", () => {
-  it("should ...", () => {
-    // implement your first test
-    fizzBuzz();
+  it("should return 1 if passed an an array with only 1 as element", () => {
+    const init = 1;
+    const expected = [1];
+
+    const actual = fizzBuzz(init);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return correct array when passed number up until 3", () => {
+    const init = 3;
+    const expected = [1, 2, "fizz"];
+
+    const actual = fizzBuzz(init);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return correct array when passed number up until 15", () => {
+    const init = 15;
+    const expected = [
+      1,
+      2,
+      "fizz",
+      4,
+      "buzz",
+      "fizz",
+      7,
+      8,
+      "fizz",
+      "buzz",
+      11,
+      "fizz",
+      13,
+      14,
+      "fizzbuzz",
+    ];
+
+    const actual = fizzBuzz(init);
+
+    expect(actual).toEqual(expected);
   });
 });
--- src/fizzBuzz/fizzBuzz.ts
+++ src/fizzBuzz/fizzBuzz.ts
@@ -1,1 +1,14 @@
-// start to implement here
+export const fizzBuzz = (init: number) => {
+  const array = Array.from({ length: init }, (_, index) => index + 1);
+
+  return array.map((elem) => {
+    if (elem % 5 === 0 && elem % 3 === 0) return "fizzbuzz";
+    if (elem % 3 === 0) {
+      return "fizz";
+    }
+    if (elem % 5 === 0) {
+      return "buzz";
+    }
+    return elem;
+  });
+};
